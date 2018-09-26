36e6924d71e715674921c6215df28ce30d1afe89
zhangchunyang <zhangchunyang@okjiaoyu.cn>
2018-09-26 14:32:06 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    base:'/fastweb-docs/',
+    themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ]
+  }
+}
