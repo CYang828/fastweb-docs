e93b4f445a144662cda8ada3be02d4ba9c28b686
zhangchunyang <zhangchunyangpri@gmail.com>
2018-09-26 22:55:15 +0800

    docs structure

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,104 @@
 module.exports = {
-    base:'/fastweb-docs/',
+    base: '/fastweb-docs/',
+    title: 'Fastweb Documentation',
+    description: 'A efficient web-server integration solution',
+    head: [
+        [
+            'link',
+            {
+                rel: 'icon',
+                href: '/favicon.png',
+                type: 'image/x-icon'
+            }
+        ]
+    ],
     themeConfig: {
         locales: {
             '/': {
-                lang: 'English',
-                title: 'Fastweb Documentation',
-                description: 'A efficient web-server integration solution',
+                label: 'English',
+                selectText: 'Languages',
+                editLinkText: 'Edit this page on GitHub',
                 nav: [
                     {
-                        text: 'en'
-                        { text: 'Index', link: '/' },
-                        { text: 'Guide', link: '/guide/' },
+                        text: 'Guide',
+                        link: '/guide/'
+                    },
+                    {
+                        text: 'Features',
+                        link: '/features/'
                     }
-                    ]
+                ],
+                sidebar: {
+                    '/guide/': [
+                        {
+                            title: 'Essentials',
+                            collapsable: false,
+                        },
+                        {
+                            title: 'Advanced',
+                            collapsable: false,
+                        },
+                        {
+                            title: 'Other',
+                            collapsable: false,
+                            children: [
+                                '/guide/other/gitter.md',
+                                '/guide/other/release-notes.md'
+                            ]
+                        }
+                    ],
+                    '/feature/component/': [
+                        '/feature/component/rich-editor.md',
+                        '/feature/component/svg-icon.md',
+                        '/feature/component/clipboard.md'
+                    ],
+                    '/feature/script/': ['/feature/script/svgo.md']
+                }
             },
             '/zh/': {
-              lang: '中文',
-              title: 'Fastweb 中文文档',
-              description: '快速构建、高效开发的后端框架'
+                title: 'Fastweb 中文文档',
+                description: '快速构建、高效开发',
+                label: '简体中文',
+                selectText: '选择语言',
+                editLinkText: '在 GitHub 上编辑此页',
+                nav: [
+                    {
+                        text: '指南',
+                        link: '/zh/guide/'
+                    },
+                    {
+                        text: '功能',
+                    }
+                ],
+                sidebar: {
+                    '/zh/guide/': [
+                        {
+                            title: '基础',
+                            collapsable: false,
+                        },
+                        {
+                            title: '进阶',
+                            collapsable: false,
+                        }
+                    ],
+                    '/zh/feature/component/': [
+                        '/zh/feature/component/rich-editor.md',
+                        '/zh/feature/component/svg-icon.md',
+                        '/zh/feature/component/clipboard.md'
+                    ],
+                    '/zh/feature/script/': ['/zh/feature/script/svgo.md']
+                }
             }
         }
     },
-
+    locales: {
+    '/': {
+      lang: 'en-US',
+      description: 'A efficient web-server integration solution'
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      description: '快速构建、高效开发'
+    }
+  },
 }
--- docs/README.md
+++ docs/README.md
@@ -4,17 +4,17 @@ heroImage: /logo.png
 actionText: Get Started →
 actionLink: /guide/
 features:
-- title: Feature-rich
+- title: Fast
   details: Minimal setup with markdown-centered project structure helps you focus on writing.
-- title: Fastweb-Powered
+- title: Versatile
   details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
-- title: Performant
+- title: Efficient
   details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
-- title: Feature-rich
+- title: Highly Components
   details: Minimal setup with markdown-centered project structure helps you focus on writing.
-- title: Fastweb-Powered
+- title: All In One
   details: Enjoy the dev experience of Vue + webpack, use Vue components in markdown, and develop custom themes with Vue.
-- title: Performant
+- title: Lazy On
   details: VuePress generates pre-rendered static HTML for each page, and runs as an SPA once a page is loaded.
 footer: Apache 2 Licensed | Copyright © 2018-present bslience
 ---
